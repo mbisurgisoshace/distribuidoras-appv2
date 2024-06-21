91736153244bd72d0edffb70f34cd61565316d3e
mbisurgisoshace <maximiliano.bisurgi@gmail.com>
2024-06-21 11:43:38 -0300

    Usar JS Navigator para reproducir efecto modal en Android

--- app/(auth)/pedidos/_layout.tsx
+++ app/(auth)/pedidos/_layout.tsx
@@ -1,6 +1,38 @@
-import { Stack } from "expo-router";
+import {
+  createStackNavigator,
+  StackNavigationOptions,
+  TransitionPresets,
+} from "@react-navigation/stack";
+import { Platform } from "react-native";
+import { withLayoutContext, Stack } from "expo-router";
+
+const { Navigator } = createStackNavigator();
+
+export const JsStack = withLayoutContext<
+  StackNavigationOptions,
+  typeof Navigator
+>(Navigator);
 
 const PedidosPage = () => {
+  if (Platform.OS === "android")
+    return (
+      <JsStack>
+        <JsStack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <JsStack.Screen
+          name="[id]"
+          options={{
+            ...TransitionPresets.ModalPresentationIOS,
+            presentation: "modal",
+          }}
+        />
+      </JsStack>
+    );
+
   return (
     <Stack>
       <Stack.Screen

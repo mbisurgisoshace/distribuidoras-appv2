4676470a3b582036b563889da49cadb4062b5fcc
mbisurgisoshace <maximiliano.bisurgi@gmail.com>
2024-08-12 02:19:32 -0300

    Filtrar pedidos por todos o pendientes

--- components/PedidosList.tsx
+++ components/PedidosList.tsx
@@ -1,3 +1,11 @@
+import {
+  Radio,
+  RadioIcon,
+  RadioLabel,
+  RadioGroup,
+  RadioIndicator,
+} from "@gluestack-ui/themed";
+import { Circle } from "lucide-react-native";
 import { useUser } from "@clerk/clerk-expo";
 import { Ionicons } from "@expo/vector-icons";
 import { useFocusEffect } from "expo-router";
@@ -16,6 +24,7 @@ export default function PedidosList() {
   const choferId = user?.publicMetadata.choferId as number;
   const [refreshing, setRefreshing] = useState(false);
   const [pedidos, setPedidos] = useState<Pedido[]>([]);
+  const [filter, setFilter] = useState<"todos" | "pendientes">("todos");
 
   useFocusEffect(
     useCallback(() => {
@@ -124,20 +133,75 @@ export default function PedidosList() {
     setPedidos(pedidos.filter((pedido) => pedido.id !== idPedido));
   };
 
+  const getFilterPedidos = () => {
+    if (filter === "todos") {
+      return pedidos;
+    }
+
+    return pedidos.filter((pedido) => pedido.estado === "Pendiente");
+  };
+
   return (
-    <SwipeListView
-      data={pedidos}
-      leftOpenValue={50}
-      disableLeftSwipe={true}
-      renderItem={renderItem}
-      renderHiddenItem={renderHiddenItem}
-      contentContainerStyle={{ paddingBottom: 30 }}
-      keyExtractor={(item) => item.id.toString()}
-      style={{ backgroundColor: "transparent", padding: 16 }}
-      ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
-      refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-      }
-    />
+    <View>
+      <RadioGroup
+        value={filter}
+        onChange={setFilter}
+        style={{
+          marginTop: 5,
+          borderWidth: 1.5,
+          borderRadius: 5,
+          paddingVertical: 8,
+          marginHorizontal: 16,
+          paddingHorizontal: 16,
+          flexDirection: "row",
+          borderColor: "#6c47ff",
+
+          justifyContent: "space-between",
+        }}
+      >
+        <Radio value="todos" size="md">
+          <RadioIndicator borderColor="#6c47ff">
+            <RadioIcon
+              as={() => (
+                <Circle
+                  size={10}
+                  color="#6c47ff"
+                  style={{ backgroundColor: "#6c47ff", borderRadius: 100 }}
+                />
+              )}
+            />
+          </RadioIndicator>
+          <RadioLabel style={{ marginLeft: 10 }}>Todos</RadioLabel>
+        </Radio>
+        <Radio value="pendientes" size="md">
+          <RadioIndicator borderColor="#6c47ff">
+            <RadioIcon
+              as={() => (
+                <Circle
+                  size={10}
+                  color="#6c47ff"
+                  style={{ backgroundColor: "#6c47ff", borderRadius: 100 }}
+                />
+              )}
+            />
+          </RadioIndicator>
+          <RadioLabel style={{ marginLeft: 10 }}>Pendientes</RadioLabel>
+        </Radio>
+      </RadioGroup>
+      <SwipeListView
+        data={getFilterPedidos()}
+        leftOpenValue={50}
+        disableLeftSwipe={true}
+        renderItem={renderItem}
+        renderHiddenItem={renderHiddenItem}
+        contentContainerStyle={{ paddingBottom: 70 }}
+        keyExtractor={(item) => item.id.toString()}
+        style={{ backgroundColor: "transparent", padding: 16 }}
+        ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+    </View>
   );
 }
